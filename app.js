a5afae9806b4a68392fd737b78afda3612321962
swolepenguin <ashtonm223@gmail.com>
2020-10-21 15:36:47 -0400

    commiting objects

---
+++ app.js
@@ -0,0 +1,9 @@
+const iphone12= {
+    image: 'path to where th image is located',
+    length: '11,28',
+    title: 'iphone12 unboxing experience + Magsafe ...',
+    username: 'Marques Brownley',
+    views:'5.5M',
+    uploaded:15
+}; 
+console.log(iphone12);
---
+++ starwars.js
@@ -0,0 +1,9 @@
+const starwars = {
+    image: 'star wars iamge',
+    platform: 'Disney+',
+    views: '1M views',
+    posted: '1D'
+
+}
+
+console.log(starwars.length)
